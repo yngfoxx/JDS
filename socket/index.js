a61f035017b741357627d9d62698c52c0c90e36f
itsYoungFox <47150226+itsYoungFox@users.noreply.github.com>
2020-12-16 22:46:32 +0000

    Creation of sockets

---
+++ socket/index.js
@@ -0,0 +1,17 @@
+const app = require('express')()
+const http = require('http').createServer(app)
+
+app.get('/', (req, res) => {
+   res.send("Running...")
+})
+
+//Socket Logic
+const socketio = require('socket.io')(http)
+
+socketio.on("connection", (userSocket) => {
+    userSocket.on("send_message", (data) => {
+        userSocket.broadcast.emit("receive_message", data)
+    })
+})
+
+http.listen(process.env.PORT)
