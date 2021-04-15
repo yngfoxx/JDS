ebddfcb143b205957f374a858a142baf26ca52ee
Stephen Osunrinde <47150226+itsYoungFox@users.noreply.github.com>
2021-04-15 05:29:21 +0100

    Updated app.py

--- application/app.py
+++ application/app.py
@@ -24,7 +24,7 @@ wSocket = socket.websocketserver(5678);
 
 domainObject = domainName()
 hostdomain = str(domainObject.getDomain())
-jds_server_domain = "aba54c63a89d.ngrok.io"
+jds_server_domain = "daff2258ecd6.ngrok.io"
 
 
 
