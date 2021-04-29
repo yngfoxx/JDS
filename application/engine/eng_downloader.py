4cce338c8b30eb425f73e0e7aec90d4c434513e8
Stephen Osunrinde <47150226+itsYoungFox@users.noreply.github.com>
2021-04-29 10:53:46 +0100

    Fixed variable bug

--- application/engine/eng_downloader.py
+++ application/engine/eng_downloader.py
@@ -18,6 +18,8 @@ from concurrent.futures import ThreadPoolExecutor
 from engine.eng_standard import stdlib
 from engine.eng_platform import domainName
 
+
+serverDomain = domainName()
 stdlib = stdlib()
 _executor = ThreadPoolExecutor(1)
 
@@ -117,7 +119,7 @@ class downloadManagerSS():
 
 
         # Paths
-        url = 'http://'+str(domainName.getServerDomain())+'/JDS/storage/'+jointID+'/'+requestID+'/Arch_'+jointID+'_'+requestID+'.zip'
+        url = 'http://'+str(serverDomain.getServerDomain())+'/JDS/storage/'+jointID+'/'+requestID+'/Arch_'+jointID+'_'+requestID+'.zip'
 
         # Create storage folder if missing
         if os.path.exists("storage") == False:
