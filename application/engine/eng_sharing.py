414472ea87d9e4d98ddfa854c80c9fb6fc575631
Stephen Osunrinde <47150226+itsYoungFox@users.noreply.github.com>
2021-04-30 16:20:00 +0100

    Updated file sharing

--- application/engine/eng_sharing.py
+++ application/engine/eng_sharing.py
@@ -155,16 +155,17 @@ class sharingManagerSS():
                         uri = "http://"+usrIPaddress+":8000/storage/"+J0INT+"/jconf.json"
                         print('[!] J0INT config uri:', uri)
                         reqJCONF = requests.get(uri)
-                        thPayload = reqJCONF.text
+                        thPayload = json.loads(reqJCONF.text)
+                        thPayload.append({"source": usrIPaddress})
 
-                        self.sharingManager(json.loads(thPayload))
+                        self.sharingManager(thPayload)
 
-                        logger = open('log_'+J0INT+'.txt', 'w')
-                        logger.write(thPayload)
+                        # logger = open('log_'+J0INT+'.txt', 'w')
+                        # logger.write(json.dumps(thPayload))
         print('\n')
 
 
-    # Thread object ----------------------------------------------------------->
+    # Thread worker ----------------------------------------------------------->
     def worker(self):
         while True:
             sQueueItem = self.sharingQueue.get()
@@ -185,8 +186,18 @@ class sharingManagerSS():
     # Queue manager ----------------------------------------------------------->
     def sharingManager(self, dArg):
         workerIndex = 0
-        for dParm in dArg:
-            self.sharingQueue.put(dParm)
+        goalSource = None
+        jointArr = []
+
+        for x in dArg:
+            if 'source' in x:
+                goalSource = x['source']
+            else:
+                jointArr.append(x)
+
+        for sParm in jointArr:
+            sParm['source'] = goalSource
+            self.sharingQueue.put(sParm)
             print("[!] Shared file added to que")
 
             workerThread = threading.Thread(target=self.worker, name=f'worker_{workerIndex}', daemon=True)
@@ -201,8 +212,189 @@ class sharingManagerSS():
 
     # File seeker ------------------------------------------------------------->
     async def fileSeeker(self, arg):
-        log = open('share_log.txt', 'a')
-        log.write(json.dumps(arg)+'\n')
-        log.close()
-        await asyncio.sleep(4)
+        if self.ws != None:
+            print('[!] WebSocket is accessible:', self.ws)
+
+        # Arguments ---------------------------------------------------------------\/
+        jointID = arg['jid']
+        requestID = arg['rid']
+        chunkID = arg['cid']
+        orderID = arg['oid']
+        uDomain = arg['source']
+
+
+        # Create storage folder if missing
+        if os.path.exists("storage") == False:
+            os.mkdir("storage")
+
+        # Create J0INT directory if missing
+        storage = "storage/"+jointID
+        if os.path.exists(storage) == False:
+            os.mkdir(storage)
+
+        # Create request directory if missing
+        storage = "storage/" + jointID + "/" + requestID
+        if os.path.exists(storage) == False:
+            os.mkdir(storage)
+
+
+        uri = "http://"+uDomain+":8000/storage/" + jointID + "/" + requestID + "/Chnk_"+jointID+"_"+requestID+"_"+chunkID+"_"+orderID+".J0INT"
+        dest = "storage/" + jointID + "/" + requestID
+        seekCONF = dest + "/config.json"
+
+
+        # Chunk/Seek config data
+        seekJSON = {}
+        seekJSON['jointID'] = arg['jid']
+        seekJSON['requestID'] = arg['rid']
+        seekJSON['chunkID'] = arg['cid']
+        seekJSON['orderID'] = arg['oid']
+        seekJSON['uDomain'] = arg['source']
+
+
+        # GET data from existing J0INT config file
+        if os.path.exists(seekCONF) == True:
+            with open(seekCONF, "r") as chConf:
+                lineIndex = 0
+                for line in chConf:
+                    skJSON = json.loads(line)
+                    if jointID == str(skJSON['jid']) and requestID == str(skJSON['rid']) and orderID == str(skJSON['id']):
+                        skNAME = "Chnk_"+jointID+"_"+requestID+"_"+chunkID+"_"+orderID+".J0INT";
+                        # check if chunk is valid
+                        if os.path.exists(chunkPATH) == True:
+                            # compare size and hash stored in config.json
+                            # to the available file in chunkPATH
+                            print('[+]', skNAME, '~ File match found\n')
+
+                            storedHash = skJSON['hash']['md5']
+                            print('[!] Stored md5 hash:', storedHash)
+
+                            calcHash = stdlib.md5(chunkPATH)
+                            print('[!] Calculated md5 hash:', calcHash)
+
+                            if storedHash == calcHash:
+                                print("[!] File exists, exiting...")
+                                # Edit config.json, Append missing parameters
+                                # REF: https://www.kite.com/python/answers/how-to-edit-a-specific-line-in-a-text-file-in-python#:~:text=Use%20file.,at%20a%20certain%20line%20number.
+                                # revalidate file data based (due to hash authenticity)
+
+                                skJSON['size'] = os.path.getsize(chunkPATH)
+                                skJSON['progress'] = 100.0
+                                skJSON['status'] = "finished"
+                                skJSON['action'] = 'realtime_share_progress'
+                                wsPayload = json.dumps(skJSON)
+
+                                # Get line to edit in config.json
+                                configFile = open(seekCONF, 'r')
+                                configLines = configFile.readlines()
+                                configLines[lineIndex] = wsPayload+'\n'
+
+                                # Set edited line in config.json
+                                configFile = open(seekCONF, 'w')
+                                configFile.writelines(configLines)
+                                configFile.close()
+
+                                # Notify socket server of download completion
+                                if self.connected == True:
+                                    await self.ws.send(wsPayload)
+                                    print('[!] Sent (Shared) realtime data ~ Already exists')
+                                else:
+                                    print('[!] File sharing manager socket is not connected')
+                                print('-'*101)
+                                return
+                    lineIndex += 1
+        else:
+            # Generate config file
+            wEngine_config = open(seekCONF, "w")
+            wEngine_config.close()
+        # -------------------------------------------------------------------------/\
+
+
+        # Validate uri ------------------------------------------------------------\/
+        headers_arg = {"Range" : "bytes=0-100"}
+        r = requests.get(uri, headers=headers_arg)
+        if r.status_code >= 400:
+            print("[-] Shared file status returned: ", r.status_code)
+            return
+        # -------------------------------------------------------------------------/\
+
+
+        print('\n[+] ->', seekJSON['uDomain'], '<-', '-'*75)
+        print(' URI\t', uri)
+        # Smart downloader --------------------------------------------------------\/
+        fileDLM = SmartDL(uri, dest)
+        fileDLM.start(blocking=False)
+
+        while not fileDLM.isFinished():
+            # MAIN FUNCTIONS =>
+            # 1. Inform socket server of download progress
+            # 2. Check if a pause request has been sent to this thread by socket communication
+            # 3. Check if an unpause request was sent to this thread by socket communication
+
+            seekJSON['size'] = fileDLM.get_dl_size()
+            seekJSON['status'] = fileDLM.get_status()
+            seekJSON['eta'] = fileDLM.get_eta(human=True)
+            seekJSON['progress'] = (fileDLM.get_progress() * 100)
+            if self.ws != None:
+                wsJSON = seekJSON
+                wsJSON['action'] = 'realtime_share_progress'
+                wsPayload = json.dumps(wsJSON)
+                try:
+                    await self.ws.send(wsPayload)
+                    print('[!] Sent (Shared) realtime data ~ Downloading')
+                except Exception as e:
+                    print('\n\n[-] Error while sending RealTime (shared) data:', e)
+
+            await asyncio.sleep(random.random() * 3)
+
+        if fileDLM.isSuccessful():
+            # MAIN FUNCTIONS =>
+            # 1. Inform socket server of thread download progress
+            seekJSON['size'] = fileDLM.get_dl_size()
+            seekJSON['status'] = fileDLM.get_status()
+            seekJSON['eta'] = fileDLM.get_eta(human=True)
+            seekJSON['progress'] = (fileDLM.get_progress() * 100)
+            seekJSON['time_elapsed'] = fileDLM.get_dl_time(human=True)
+            seekJSON['hash'] = {
+                'md5' : fileDLM.get_data_hash('md5'),
+                'sha1' : fileDLM.get_data_hash('sha1'),
+                'sha256' : fileDLM.get_data_hash('sha256'),
+            }
+            if self.ws != None:
+                wsJSON = seekJSON
+                wsJSON['action'] = 'realtime_share_progress'
+                wsPayload = json.dumps(wsJSON)
+                try:
+                    await self.ws.send(wsPayload)
+                    print('[!] Sent (Shared) realtime data ~ Finished')
+                except Exception as e:
+                    print('\n\n[-] Error while sending RealTime (shared) data:', e)
+
+
+        else:
+            print("[!] There were some errors:")
+            # MAIN FUNCTIONS =>
+            # 1. Inform socket server of thread download error
+
+            for e in fileDLM.get_errors():
+                print(str(e))
+
+            if self.ws != None:
+                seekJSON['error'] = fileDLM.get_errors()
+                seekJSON['action'] = 'realtime_share_progress'
+                wsPayload = json.dumps(seekJSON)
+                try:
+                    await self.ws.send(wsPayload)
+                    print('[!] Sent (Shared) realtime data ~ Error')
+                except Exception as e:
+                    print('\n\n[-] Error while sending RealTime (shared) data:', e)
+
+        # log = open('share_log.txt', 'a')
+        # log.write(json.dumps(arg)+'\n')
+        # log.close()
+        # await asyncio.sleep(4)
+        path = fileDLM.get_dest()
+        print('[+] Done ~', path)
+
+        print('[+]', '-'*97, '\n')
     # ------------------------------------------------------------------------->
