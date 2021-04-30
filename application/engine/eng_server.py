758732fd9f6b9f7f81969b035669a6944b07c412
Stephen Osunrinde <47150226+itsYoungFox@users.noreply.github.com>
2021-04-30 16:22:16 +0100

    Updated HTTP server to accept share request

--- application/engine/eng_server.py
+++ application/engine/eng_server.py
@@ -52,6 +52,15 @@ class LocalServer(SimpleHTTPRequestHandler):
                 f.close()
                 return
 
+            elif self.path.endswith(".J0INT"):
+                f = open(curdir + sep + self.path, "rb")
+                self.send_response(200)
+                self.send_header("Content-type", "text/J0INT")
+                self.end_headers()
+                self.wfile.write(f.read())
+                f.close()
+                return
+
             elif self.path.endswith(".less"):
                 f = open(curdir + sep + self.path, "rb")
                 self.send_response(200)
